8f5fe50a7c0408125d9f5f32170c85143d5a1a58
Herbie Vine <me@herbievine.com>
2024-04-08 11:45:40 +0200

    feat: tcp server

--- main.go
+++ main.go
@@ -1,31 +1,85 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
+
+type Message struct {
+	from  net.Addr
+	bytes []byte
+}
 
 type Server struct {
 	listenAddr string
 	ln         net.Listener
-	quitch     chan struct{}
+	msgch      chan Message
 }
 
-func NewServer(listenAddr string) *Server {
+func newServer(listenAddr string) *Server {
 	return &Server{
 		listenAddr: listenAddr,
-		quitch:     make(chan struct{}),
+		msgch:      make(chan Message, 10),
 	}
 }
 
-func (s *Server) Start() error {
+func (s *Server) start() error {
 	ln, err := net.Listen("tcp", s.listenAddr)
 	if err != nil {
 		return err
 	}
 
+	fmt.Println("Listening on", s.listenAddr)
+
 	defer ln.Close()
+	defer close(s.msgch)
+
+	go s.accept()
 
 	s.ln = ln
 
-	<-s.quitch
-
 	return nil
+}
+
+func (s *Server) accept() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			continue
+		}
+
+		go s.read(conn)
+	}
+}
+
+func (s *Server) read(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			continue
+		}
+
+		s.msgch <- Message{
+			from:  conn.RemoteAddr(),
+			bytes: buf[:n],
+		}
+	}
+}
+
+func main() {
+	server := newServer(":6969")
+
+	err := server.start()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for msg := range server.msgch {
+		fmt.Printf("Message received from (%s): %s\n", msg.from, string(msg.bytes))
+	}
 }
